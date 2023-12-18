39224292b1587614d0550200b3816ef0b497d457
StefanIliev545 <stefan@obscu.ro>
2023-12-18 13:40:59 +0200

    Change for other stuck eth.

--- contracts/deployment_scripts/testnet/recoverfunds/002_change_admin.ts
+++ contracts/deployment_scripts/testnet/recoverfunds/002_change_admin.ts
@@ -3,11 +3,15 @@ import {DeployFunction} from 'hardhat-deploy/types';
 
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-    return;
     const {deployer} = await hre.getNamedAccounts();
     const ethers = hre.ethers;
-    const proxyAdmin =await ethers.getContractAt('IDefaultProxyAdmin', '0xFFa0feA0cb522f3d0CE644B9a111215ACbEb061B', await ethers.getSigner(deployer))
-    const tx = await proxyAdmin.changeProxyAdmin('0xEde0fC70C4B67916B8d2037dE24cD18BF26e5069', '0xeA052c9635F1647A8a199c2315B9A66ce7d1e2a7')
+
+    const proxyAdminAddr = '0x9c46fb7c0fe2effc0795046d142c89c4c09fc1c6';
+    const proxyAddr = '0x9a3031a39a34887516b66b455c490c6eb8d048ee';
+    const safeWalletAddr = '0xeA052c9635F1647A8a199c2315B9A66ce7d1e2a7';
+
+    const proxyAdmin =await ethers.getContractAt('IDefaultProxyAdmin', proxyAdminAddr, await ethers.getSigner(deployer))
+    const tx = await proxyAdmin.changeProxyAdmin(proxyAddr, safeWalletAddr)
     const receipt = await tx.wait();
     if (receipt.status != 1) {
         console.error('Unable to change proxy admin');
